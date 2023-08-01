0b0e50aeb3111e0c5e17e35ea80d1ac2e0b50570
PalmkvistJoakim <jockepalmkvist@gmail.com>
2023-08-01 14:49:02 +0200

    teest

--- src/components/home/homeBackgroundWithLinks/HomeBackgroundWithLinks.tsx
+++ src/components/home/homeBackgroundWithLinks/HomeBackgroundWithLinks.tsx
@@ -4,7 +4,7 @@ import Button from "../../common/button/Button";
 function HomeBackgroundWithLinks() {
   return (
     <div className="home-background-with-links">
-      <img src="/images/2023-231.jpg" />
+      <img src="/images/Stjarnorter_logga1.png" />
       <p>
         Upplev Hälsa & Harmoni!
         <br /> Utforska vårt sortiment av produkter, behandlingar och
