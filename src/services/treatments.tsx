6757870954c5592a6cdb5aa0f0fb9a609b216ca3
PalmkvistJoakim <jockepalmkvist@gmail.com>
2023-10-24 15:56:55 +0200

    10242

--- src/services/treatments.tsx
+++ src/services/treatments.tsx
@@ -44,29 +44,30 @@ const treatments = [
           Att vandra sin livsväg utan motgångar eller prövningar är få förunnat.
           Livet går på över berg och genom djupa dalar, genom ljus och mörker
           för att vi ska växa och bli medvetna varelser.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Att göra livsresan på egen hand kan vara både tungt och svårt, en
           rådgivning kan vara ett sätt att få information till sig så man kan gå
           vidare med större tillförsikt.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Jag arbetar med kanalisering, där jag är en kanal till ett högre
           medvetande.
-        </p>
-        <p>
+          <br />
           Vem eller vad medvetandet är får jag inte alltid information om.
           Oftast är det en nära anhörig, guide eller ljusväsen. Ibland ber jag
           ombedd att använda mig av en Tarotlek för att förtydliga deras budskap
           för kunden. Det kan ibland vara lättare att läsa budskapet än att höra
           det.
+          <br />
+          <br />
+          En rådgivning tar ungefär 1,5-2 timmar.
+          <br />
+          Telefon konsultation tar ca 1 timme.
+          <br />
+          Maila eller ring för mer information.
+          <br />
         </p>
-        <br />
-        <p>En rådgivning tar ungefär 1,5-2 timmar.</p>
-        <p>Telefon konsultation tar ca 1 timme.</p>
-        <p>Maila eller ring för mer information.</p>
       </>
     ),
     logo: "images/Stjarnorter_logga1.png",
@@ -84,33 +85,30 @@ const treatments = [
           Sutherland, som på 1920-talet upptäckte att hjärnan, hjärnhinnorna,
           ryggmärgen och korsbenet med sin vätska utgör ett slutet hydrauliskt
           system.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Detta system har, precis som det cardiovasculära, sin egen ”puls”.
           Terapin balanserar stress och trauman i bla nervsystemet, höfterna,
           korsryggen, nacke och huvud.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           I en frisk kropp ligger pulsationen på ca 6-12 svängningar per minut.
           Denna puls eller rörelse skapar mycket små röresler i hela kroppen.
           Vid en blockering av denna craniala puls ex i nacken kan det leda till
           huvudvärk och spänningar i huvudet. Genom att hjälpa kroppen att
           återställa rytmen och pulsen kan därmed kroppens naturliga balans
           återskapas.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Behandlingen börjar vid fötterna och fortsätter upp över ben, höfter,
           korsrygg, nacke och huvud. Klienten ligger på rygg under hela
           behandlingen. Mjuka, löst sittande kläder rekommenderas.
-        </p>
-        <p style={{ fontWeight: "bold" }}>
-          Denna behandlingsteknik lämpar sig också mycket väl för djur.
-        </p>
-        <br />
-        <p>
+          <br />
+          <span style={{ fontWeight: "bold" }}>
+            Denna behandlingsteknik lämpar sig också mycket väl för djur.
+          </span>
+          <br />
+          <br />
           Olika tillstånd som Cranio sacrala terapin kan vara lämplig för är
           whiplash-skador, värk i nacke, axlar och rygg , korsryggs- och
           höftproblem. Andra besvär kan vara huvudvärk, migrän , fibromyalgi,
@@ -131,35 +129,38 @@ const treatments = [
           Balansering av kroppens energisystem! Aditimetoden är en mycket
           djupgående energibalanseringsteknik där kroppens samtliga energisystem
           balanseras.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Den yttersta grundorsaken till allt lidande lokaliseras och
           elimineras.
-        </p>
-        <br />
-        <p style={{ fontWeight: "bold" }}>
-          Exempel på vad som kan uppnås med en aditibalansering:
-        </p>
-        <br />
-        <p>• Ökad tillgång till kreativitet och livsglädje.</p>
-        <br />
-        <p>• Ökad intuition.</p>
-        <br />
-        <p>
+          <br />
+          <br />
+          <span style={{ fontWeight: "bold" }}>
+            Exempel på vad som kan uppnås med en aditibalansering:
+          </span>
+          <br />
+          <br />
+          • Ökad tillgång till kreativitet och livsglädje.
+          <br />
+          <br />
+          • Ökad intuition.
+          <br />
+          <br />
           • Frihet från rädslor av olika slag, ex flygrädsla, ångestattacker.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           • Ökat immunförsvar,(då stressnivån i kroppen minskar) och därmed
           minskad benägenhet för infektioner.
+          <br />
+          <br />
+          • Lindring eller befrielse från fysiska besvär.
+          <br />
+          <br />
+          • Ökad förmåga att förstå, förlåta och känna villkorslös kärlek.
+          <br />
+          <br />
+          (Aditi = utan gräns!)
         </p>
-        <br />
-        <p>• Lindring eller befrielse från fysiska besvär.</p>
-        <br />
-        <p>• Ökad förmåga att förstå, förlåta och känna villkorslös kärlek.</p>
-        <br />
-        <p>(Aditi = utan gräns!)</p>
       </>
     ),
     logo: "images/Stjarnorter_logga1.png",
@@ -174,15 +175,13 @@ const treatments = [
           Bonitologin hjälper dig att läka dig själv. Med kinesologi testas
           grundorsakerna till fysiska, mentala, emotionella eller andliga
           problem och ett behandlingsprogram skräddarsys för just dig.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Din kropp visar själv exakt vad som krävs för att en läkningsprocess
           skall starta eller vad som behövs för att behålla en optimal hälsa
           oberoende av sjukdomsproblemets art.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Hos en Bonitolog får Du hjälp med alla typer av obalanser i ditt
           hälsotillstånd. Från enklare förkylningssymptom till så kallade
           ”kroniska” åkommor, eller om Du på annat sätt inte är i harmoni med
@@ -191,9 +190,10 @@ const treatments = [
           indianstammarnas schamaner. Homeopati, vitamin- och mineralterapi,
           naturmedel, immunbiologiska medel, healing och energiarbete i olika
           former.
-        </p>
-        <p style={{ fontWeight: "bold" }}>
-          Bonitologi är en behandlingsform för såväl människor som djur.
+          <br />
+          <span style={{ fontWeight: "bold" }}>
+            Bonitologi är en behandlingsform för såväl människor som djur.
+          </span>
         </p>
       </>
     ),
@@ -209,9 +209,8 @@ const treatments = [
           Regndroppsteknik är en behaglig, avstressande och djupgående
           massagemetod, där man använder sig av olika sorters eteriska oljor av
           mycket hög kvalité.
-        </p>
-        <br />
-        <p>
+          <br />
+          <br />
           Oljorna masseras in på föttterna med hjälp av vitaflexrörelser.
           Terapauten för in oljorna längs fotkammen med fingertopparna som
           skapar elektriska impulser till olika reflexpunkter i kroppen.
@@ -221,8 +220,9 @@ const treatments = [
           minuter för en olja att nå alla cellerna i kroppen. Som avslutning får
           klienten en behaglig ben- och nacksträckning samt en skön
           ansiktsmassage.
+          <br />
+          Behandlingen tar 1-1,5 timme.
         </p>
-        <p>Behandlingen tar 1-1,5 timme.</p>
       </>
     ),
     logo: "images/Stjarnorter_logga1.png",
